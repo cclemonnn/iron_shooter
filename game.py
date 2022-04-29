146a5d5b44cced7e28161159ca10dcd61b1d9379
cclemonnn <petersocool0313@gmail.com>
2022-04-29 18:24:31 +0800

    first basic commit

---
+++ game.py
@@ -0,0 +1,39 @@
+import pygame
+from pygame.sprite import Sprite
+
+pygame.init()
+
+# screen
+WIDTH = 1024
+HEIGHT = 768
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('mario shooter')
+
+# colors
+GREEN = (0, 255, 0)
+
+# game modes
+running = True
+
+
+class Player(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('images/all_explosions.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+
+# game loop
+while running:
+    screen.fill(GREEN)
+    # handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+pygame.quit()
