367d8c265d1660f683f687c14f1b2667a1598ff4
cclemonnn <petersocool0313@gmail.com>
2022-04-29 22:59:20 +0800

    added ultron

--- game.py
+++ game.py
@@ -46,13 +46,29 @@ class Player(Sprite):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
+class Ultron(Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('images/ultron/0.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 5
+        self.flip = False
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+
 player = Player()
+ultron = Ultron(200, 200)
 
 # game loop
 while running:
     clock.tick(FPS)
     screen.fill(GREEN)
     player.move()
+    ultron.draw()
     player.draw()
 
     # handle events
