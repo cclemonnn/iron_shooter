8dc34afc5ca3f72a4df8cc9b24b236c638fae1fc
cclemonnn <petersocool0313@gmail.com>
2022-04-30 23:08:00 +0800

    added Ultron_Health

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ pygame.display.set_caption('mario shooter')
 
 # colors
 GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 # player actions
 moving_right = False
@@ -88,7 +90,7 @@ class Laser(Sprite):
             self.rect.left = player.rect.right + 80
             self.rect.y = player.rect.y + 5
         else:
-            self.rect.right = player.rect.left
+            self.rect.right = player.rect.left + 10
             self.rect.y = player.rect.y + 5
         screen.blit(pygame.transform.flip(self.image, player.flip, False), self.rect)
 
@@ -102,9 +104,31 @@ class Ultron(Sprite):
         self.rect.y = y
         self.speed = 5
         self.flip = False
+        # health
+        self.max_health = 100
+        self.current_health = 100
+        self.health_bar = Ultron_Health(self.max_health)
+        self.alive = True
 
     def draw(self):
         screen.blit(self.image, self.rect)
+        self.health_bar.show_health_bar(self.rect.left, self.rect.top, self.rect.width, self.current_health)
+
+
+class Ultron_Health():
+    def __init__(self, max_health):
+        self.max_health = max_health
+
+    def show_health_bar(self, left, top, width, healh):
+        # draw max health
+        pygame.draw.rect(screen, RED, (left, top - 20, width, 10))
+        # draw actual health
+        ratio = healh/self.max_health
+        pygame.draw.rect(screen, BLUE, (left, top - 20, width * ratio, 10))
+
+
+
+
 
 
 laser = Laser()
